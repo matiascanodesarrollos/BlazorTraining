cf4725c2a3ad75c6eb58d13b362b086a5f689b2c
matiascanodesarrollos <cano.matias@gmail.com>
2019-10-18 14:04:10 -0300

    Initial setup.

---
+++ EventSubscriptionApp/EventSubcription.Model/DLS/EventContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventSubcription.Model.DLS
+{
+    public class EventContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=Events.db");
+    }
+}
